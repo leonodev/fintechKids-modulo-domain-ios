d3dbdc2bf48ac4070e0c1997a34d27b23943fbc7
Fredy Leon <fredy.leon@ua.la.com>
2026-02-25 11:54:01 +0100

    add supabase

--- Package.swift
+++ Package.swift
@@ -15,11 +15,19 @@ let package = Package(
             targets: ["FHKDomain"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/supabase/supabase-swift.git",
+                .upToNextMajor(from: "2.5.1"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FHKDomain"
+            name: "FHKDomain",
+            dependencies: [
+                // Modules
+                .product(name: "Supabase", package: "supabase-swift")
+            ]
         ),
         .testTarget(
             name: "FHKDomainTests",
---
+++ Sources/FHKDomain/Auth/Models/Security.swift
@@ -0,0 +1,13 @@
+//
+//  Security.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 25/2/26.
+//
+
+import Foundation
+
+public enum SecurityError: Error {
+    case cryptoError(String)
+    case invalidKeyData
+}
