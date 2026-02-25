16c7c4923be3c7e30185e0e97ee2ccd8a6aaff5c
Fredy Leon <fredy.leon@ua.la.com>
2026-02-25 20:20:52 +0100

    move protocols analytics

---
+++ Sources/FHKDomain/Observability/Models/AnalyticsEvent.swift
@@ -0,0 +1,42 @@
+//
+//  AnalyticsEvent.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 25/2/26.
+//
+
+import Foundation
+
+public enum AnalyticsEvent {
+    case screenView(Screen)
+    case tapButton(Button)
+    case error(ErrorDetail)
+    
+    public struct Screen: Equatable {
+        public let name: String
+        public let screenClass: String
+        
+        public init(name: String, screenClass: String) {
+            self.name = name
+            self.screenClass = screenClass
+        }
+    }
+    
+    public struct Button: Equatable {
+        public let name: String
+        
+        public init(name: String) {
+            self.name = name
+        }
+    }
+    
+    public struct ErrorDetail: Equatable {
+        public let type: String
+        public let message: String
+        
+        public init(type: String, message: String) {
+            self.type = type
+            self.message = message
+        }
+    }
+}
---
+++ Sources/FHKDomain/Observability/Protocols/FHKAnalyticsProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  FHKAnalyticsProtocol.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 25/2/26.
+//
+
+public protocol FHKAnalyticsProtocol {
+    func track(_ event: AnalyticsEvent)
+}
