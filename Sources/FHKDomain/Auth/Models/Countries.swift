22528d346940108be6ae6e8ba6f89f8b40ad4b77
Fredy Leon <fredy.leon@ua.la.com>
2026-02-26 11:08:18 +0100

    add model countries

---
+++ Sources/FHKDomain/Auth/Models/Countries.swift
@@ -0,0 +1,13 @@
+//
+//  Countries.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 26/2/26.
+//
+
+public enum Countries {
+    case spanish
+    case french
+    case england
+    case italy
+}
