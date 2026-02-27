67ac1fedd60a199819560ea8ec24191e74a9d7aa
Fredy Leon <fredy.leon@ua.la.com>
2026-02-27 07:17:05 +0100

    change model

--- Sources/FHKDomain/Auth/Models/UserSession.swift
+++ Sources/FHKDomain/Auth/Models/UserSession.swift
@@ -35,21 +35,18 @@ import Foundation
 public struct FHKUserSession: Sendable {
     public let id: UUID
     public let email: String
-    public let accessToken: String
-    public let refreshToken: String
-    public let expiresAt: Date?
+    public let accessToken: String?
+    public let refreshToken: String?
     
-    public init(
-        id: UUID,
-        email: String,
-        accessToken: String,
-        refreshToken: String,
-        expiresAt: Date? = nil
-    ) {
+    // Propiedad calculada para saber si realmente puede navegar por la app
+    public var hasActiveSession: Bool {
+        return accessToken != nil
+    }
+
+    public init(id: UUID, email: String, accessToken: String? = nil, refreshToken: String? = nil) {
         self.id = id
         self.email = email
         self.accessToken = accessToken
         self.refreshToken = refreshToken
-        self.expiresAt = expiresAt
     }
 }
