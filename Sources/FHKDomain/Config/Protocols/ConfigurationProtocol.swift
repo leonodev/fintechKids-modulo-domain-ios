07106a130db9d177d88112675594f1d263c77be8
Fredy Leon <fredy.leon@ua.la.com>
2026-02-28 14:19:49 +0100

    remove method from protocol

--- Sources/FHKDomain/Config/Protocols/ConfigurationProtocol.swift
+++ Sources/FHKDomain/Config/Protocols/ConfigurationProtocol.swift
@@ -11,5 +11,4 @@ public protocol FHKConfigurationProtocol: FHKInjectableProtocol {
     var environmentType: EnvironmentType { get set }
     func setEnvironment(_ environmentType: EnvironmentType)
     func getEnvironment() -> EnvironmentType
-    func getParentMail() async -> String?
 }
