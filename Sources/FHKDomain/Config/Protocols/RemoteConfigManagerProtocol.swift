736eb9fcdb9de43dfd41a743b983611f41ffbda6
Fredy Leon <fredy.leon@ua.la.com>
2026-02-28 14:55:15 +0100

    modified protocols

--- Sources/FHKDomain/Config/Protocols/RemoteConfigManagerProtocol.swift
+++ Sources/FHKDomain/Config/Protocols/RemoteConfigManagerProtocol.swift
@@ -5,6 +5,7 @@
 //  Created by Fredy Leon on 26/2/26.
 //
 
+@MainActor
 public protocol FHKRemoteConfigManagerProtocol: FHKInjectableProtocol {
     var enabledLanguages: [String] { get }
     func fetchConfig(completion: @escaping (Error?) -> Void)
