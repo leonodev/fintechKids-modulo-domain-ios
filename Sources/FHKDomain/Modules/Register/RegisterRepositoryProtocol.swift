6ab28be866f5b1a571d9b966bd20d8ff4a650de9
Fredy Leon <fredy.leon@ua.la.com>
2026-03-02 21:05:44 +0100

    add injectable

--- Sources/FHKDomain/Modules/Register/RegisterRepositoryProtocol.swift
+++ Sources/FHKDomain/Modules/Register/RegisterRepositoryProtocol.swift
@@ -5,7 +5,7 @@
 //  Created by Fredy Leon on 2/3/26.
 //
 
-public protocol RegisterRepositoryProtocol {
+public protocol RegisterRepositoryProtocol: FHKInjectableProtocol {
     func register(email: String, password: String) async throws -> FHKUserSession
     func saveUserIntoKeychain() async
 }
