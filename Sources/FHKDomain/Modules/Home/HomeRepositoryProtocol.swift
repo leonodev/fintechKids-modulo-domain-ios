12b9295657510a06fc516b6e2fd80ec9f89953ec
Fredy Leon <fredy.leon@ua.la.com>
2026-03-03 09:14:32 +0100

    rename protocol

--- Sources/FHKDomain/Modules/Home/HomeRepositoryProtocol.swift
+++ Sources/FHKDomain/Modules/Home/HomeRepositoryProtocol.swift
@@ -5,6 +5,6 @@
 //  Created by Fredy Leon on 3/3/26.
 //
 
-public protocol HomeRepositoryProtocol: FHKInjectableProtocol {
+public protocol FHKHomeRepositoryProtocol: FHKInjectableProtocol {
     func fetchMembers(email: String) async throws
 }
