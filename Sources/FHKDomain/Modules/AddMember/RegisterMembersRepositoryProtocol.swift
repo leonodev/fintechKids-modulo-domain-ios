f9617d0c8c52a3caa88c8ea77f402a85d918237a
Fredy Leon <fredy.leon@ua.la.com>
2026-03-03 08:11:18 +0100

    remane protocol

---
+++ Sources/FHKDomain/Modules/AddMember/RegisterMembersRepositoryProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  RegisterMembersRepositoryProtocol.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 3/3/26.
+//
+
+public protocol RegisterMembersRepositoryProtocol: FHKInjectableProtocol {
+    func registerMembers()
+}
