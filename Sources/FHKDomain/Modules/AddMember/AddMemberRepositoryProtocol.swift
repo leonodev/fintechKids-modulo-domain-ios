a1dc15a77af8d12a4e9441befc78a2fc0ba05955
Fredy Leon <fredy.leon@ua.la.com>
2026-03-03 08:09:41 +0100

    add AddMemberRepositoryProtocol

---
+++ Sources/FHKDomain/Modules/AddMember/AddMemberRepositoryProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  AddMemberRepositoryProtocol.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 3/3/26.
+//
+
+public protocol AddMemberRepositoryProtocol: FHKInjectableProtocol {
+    func registerMembers()
+}
