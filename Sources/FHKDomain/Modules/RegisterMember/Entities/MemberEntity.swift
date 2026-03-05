7cb2c655ffd3977f780e5fdb36c6aaa8b38ada39
Fredy Leon <fredy.leon@ua.la.com>
2026-03-05 22:30:05 +0100

    add MemberEntity

---
+++ Sources/FHKDomain/Modules/RegisterMember/Entities/MemberEntity.swift
@@ -0,0 +1,28 @@
+//
+//  MemberEntity.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 5/3/26.
+//
+
+import Foundation
+import SwiftUI
+
+public struct MemberEntity: Identifiable, Hashable, Equatable {
+    public let id: UUID
+    public let emailParent: String
+    public let memberName: String
+    public let avatarName: String
+    public var iconName: String = "trash"
+    
+    public init(id: UUID,
+                emailParent: String,
+                memberName: String,
+                avatarName: String = "boy_9"
+    ) {
+        self.id = id
+        self.emailParent = emailParent
+        self.memberName = memberName
+        self.avatarName = avatarName
+    }
+}
