afc98f2627bf338dbb2322c578ddaa0ae85ee6f5
Fredy Leon <fredy.leon@ua.la.com>
2026-03-07 10:46:44 +0100

    add new method

--- Sources/FHKDomain/Config/Protocols/FHKConfigurationProtocol.swift
+++ Sources/FHKDomain/Config/Protocols/FHKConfigurationProtocol.swift
@@ -9,6 +9,8 @@ import Foundation
 
 public protocol FHKConfigurationProtocol: FHKInjectableProtocol {
     var parentMail: String? { get set }
+    func updateParentMail()
+    
     var environmentType: EnvironmentType { get set }
     func setEnvironment(_ environmentType: EnvironmentType)
     func getEnvironment() -> EnvironmentType
