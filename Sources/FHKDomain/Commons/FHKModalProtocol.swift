b640562acd662a1b7be354e14eefec01864e5912
Fredy Leon <fredy.leon@ua.la.com>
2026-03-07 13:20:07 +0100

    add new protocol

---
+++ Sources/FHKDomain/Commons/FHKModalProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  ModalProtocol.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 7/3/26.
+//
+
+import SwiftUI
+
+public protocol FHKModalProtocol: AnyObject, Sendable {
+    var isPresented: Bool { get set }
+    var content: AnyView? { get set }
+    func show<V: View>(@ViewBuilder _ content: () -> V)
+    func dismiss()
+}
