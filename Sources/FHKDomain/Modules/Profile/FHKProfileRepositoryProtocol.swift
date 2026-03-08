8476cb94d07d4bfa2b4e4ad4cb027ecf59bbfeb2
Fredy Leon <fredy.leon@ua.la.com>
2026-03-08 07:48:43 +0100

    add ProfileRepositoryProtocol

---
+++ Sources/FHKDomain/Modules/Profile/FHKProfileRepositoryProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  FHKProfileRepositoryProtocol.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 8/3/26.
+//
+
+public protocol FHKProfileRepositoryProtocol: FHKInjectableProtocol {
+    func logout() async throws
+    func deleteKeychain(key: String) throws
+}
