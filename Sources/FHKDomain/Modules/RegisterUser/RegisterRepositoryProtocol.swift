edbc99e8b1e0c8acf06d7102e26ec3fd727c5d20
Fredy Leon <fredy.leon@ua.la.com>
2026-03-10 15:52:22 +0100

    add method save family name

--- Sources/FHKDomain/Modules/RegisterUser/RegisterRepositoryProtocol.swift
+++ Sources/FHKDomain/Modules/RegisterUser/RegisterRepositoryProtocol.swift
@@ -7,4 +7,5 @@
 
 public protocol RegisterRepositoryProtocol: FHKInjectableProtocol {
     func register(email: String, password: String, familyName: String) async throws -> FHKUserSession
+    func saveFamilyInfoKeychain(familyName: String)
 }
