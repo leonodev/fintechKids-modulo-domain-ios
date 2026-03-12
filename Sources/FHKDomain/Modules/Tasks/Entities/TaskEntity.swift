6c496d79a176237e00ce820214f73d76c87235df
Fredy Leon <fredy.leon@ua.la.com>
2026-03-12 13:13:09 +0100

    add new entity task

---
+++ Sources/FHKDomain/Modules/Tasks/Entities/TaskEntity.swift
@@ -0,0 +1,32 @@
+//
+//  TaskEntity.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 12/3/26.
+//
+
+import Foundation
+
+public struct TaskEntity: DomainModelProtocol {
+    public let id: Int
+    public let createdAt: String
+    public let name: String
+    public let timeGranted: Int
+    public let coinsGranted: Int
+    public let emailParent: String
+    
+    init(id: Int,
+         createdAt: String,
+         name: String,
+         timeGranted: Int,
+         coinsGranted: Int,
+         emailParent: String
+    ) {
+        self.id = id
+        self.createdAt = createdAt
+        self.name = name
+        self.timeGranted = timeGranted
+        self.coinsGranted = coinsGranted
+        self.emailParent = emailParent
+    }
+}
