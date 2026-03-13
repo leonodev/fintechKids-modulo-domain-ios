b5a65bf4ae3df38a2a127d37d178160f5fac24d0
Fredy Leon <fredy.leon@ua.la.com>
2026-03-13 10:03:27 +0100

    add new property

--- Sources/FHKDomain/Modules/Tasks/Entities/TaskEntity.swift
+++ Sources/FHKDomain/Modules/Tasks/Entities/TaskEntity.swift
@@ -11,18 +11,21 @@ public struct TaskEntity: DomainModelProtocol {
     public let id: UUID = UUID()
     public let createdAt: String
     public let name: String
+    public let description: String
     public let timeGranted: Int
     public let coinsGranted: Int
     public let emailParent: String
     
     public init(createdAt: String,
                 name: String,
+                description: String,
                 timeGranted: Int,
                 coinsGranted: Int,
                 emailParent: String
     ) {
         self.createdAt = createdAt
         self.name = name
+        self.description = description
         self.timeGranted = timeGranted
         self.coinsGranted = coinsGranted
         self.emailParent = emailParent
