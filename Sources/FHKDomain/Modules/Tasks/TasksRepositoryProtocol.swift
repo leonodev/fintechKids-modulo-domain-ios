c83c1d1ee816a4b81a61be55fa437dd2343d0487
Fredy Leon <fredy.leon@ua.la.com>
2026-03-13 09:38:23 +0100

    change to async

--- Sources/FHKDomain/Modules/Tasks/TasksRepositoryProtocol.swift
+++ Sources/FHKDomain/Modules/Tasks/TasksRepositoryProtocol.swift
@@ -5,8 +5,8 @@
 //  Created by Fredy Leon on 12/3/26.
 //
 
-public protocol FHKTasksRepositoryProtocol: Sendable {
+public protocol FHKTasksRepositoryProtocol: FHKInjectableProtocol {
     func createTask(task: TaskEntity) async throws
     func getTasks(emailParent: String, forceRefresh: Bool) async throws -> [TaskEntity]
-    func clearCache()
+    func clearCache() async
 }
