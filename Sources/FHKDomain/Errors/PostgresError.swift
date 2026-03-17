24ae8e8b28c43f59a530994c0b8a03ff16753aa7
Fredy Leon <fredy.leon@ua.la.com>
2026-03-17 17:24:40 +0100

    rename file

--- Sources/FHKDomain/Errors/PostgresError.swift
+++ Sources/FHKDomain/Errors/PostgresError.swift
@@ -5,7 +5,7 @@
 //  Created by Fredy Leon on 17/3/26.
 //
 
-public struct PostgresError: Error {
+public struct FHKPostgresError: Error {
     public let code: String
     public let message: String
     public let hint: String?
