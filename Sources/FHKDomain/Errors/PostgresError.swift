c7a696bc7719c4f81600afa0387acc117cf22aaa
Fredy Leon <fredy.leon@ua.la.com>
2026-03-17 16:54:32 +0100

    chane

--- Sources/FHKDomain/Errors/PostgresError.swift
+++ Sources/FHKDomain/Errors/PostgresError.swift
@@ -9,8 +9,4 @@ public struct PostgresError: Error {
     public let code: String
     public let message: String
     public let hint: String?
-    
-    public var isInvalidDateFormat: Bool {
-        return code == "22007"
-    }
 }
