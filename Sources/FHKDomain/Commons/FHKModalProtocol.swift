e4e4be7c38ca0c8765939ae499d47bc6bf560c1f
Fredy Leon <fredy.leon@ua.la.com>
2026-03-24 22:19:03 +0100

    remove properties

--- Sources/FHKDomain/Commons/FHKModalProtocol.swift
+++ Sources/FHKDomain/Commons/FHKModalProtocol.swift
@@ -10,8 +10,6 @@ import SwiftUI
 public protocol FHKModalProtocol: AnyObject, Sendable {
     var isPresented: Bool { get set }
     var content: AnyView? { get set }
-    var updateTick: Int { get set }
-    func forceUpdate()
     func show<V: View>(onDismiss: (() -> Void)?, @ViewBuilder content: () -> V)
     func dismiss()
 }
