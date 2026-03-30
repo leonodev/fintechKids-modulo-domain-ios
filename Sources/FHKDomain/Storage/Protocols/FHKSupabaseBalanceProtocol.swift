b9432f4efc2f3757fa710bd27e1181b048cc9097
Fredy Leon <fredy.leon@ua.la.com>
2026-03-30 12:54:27 +0200

    change parameter type

--- Sources/FHKDomain/Storage/Protocols/FHKSupabaseBalanceProtocol.swift
+++ Sources/FHKDomain/Storage/Protocols/FHKSupabaseBalanceProtocol.swift
@@ -5,7 +5,9 @@
 //  Created by Fredy Leon on 30/3/26.
 //
 
+import Foundation
+
 public protocol FHKSupabaseBalanceProtocol: FHKInjectableProtocol {
     func fetchBalance(memberId: String) async throws -> BalanceEntity
-    func updateBalance(memberId: String, infoBalance: BalanceEntity) async throws
+    func updateBalance(memberId: UUID, infoBalance: BalanceEntity) async throws
 }
