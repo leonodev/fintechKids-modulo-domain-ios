892f20a6bdba323d9356b8fb7eadd0529722df18
Fredy Leon <fredy.leon@ua.la.com>
2026-04-01 11:40:33 +0200

    rename parameter protocol

--- Sources/FHKDomain/Storage/Protocols/FHKSupabaseRewardsProtocol.swift
+++ Sources/FHKDomain/Storage/Protocols/FHKSupabaseRewardsProtocol.swift
@@ -6,6 +6,6 @@
 //
 
 public protocol FHKSupabaseRewardsProtocol: FHKInjectableProtocol {
-    func createReward(goal: RewardEntity) async throws
+    func createReward(reward: RewardEntity) async throws
     func fetchRewards(emailParent: String) async throws -> [RewardEntity]
 }
