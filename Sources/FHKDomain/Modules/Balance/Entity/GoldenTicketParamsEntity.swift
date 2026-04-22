c8ec73fc29ab053e80a899c022c4bc5399489461
Fredy Leon <fredy.leon@ua.la.com>
2026-04-22 10:02:07 +0200

    change type parameter

--- Sources/FHKDomain/Modules/Balance/Entity/GoldenTicketParamsEntity.swift
+++ Sources/FHKDomain/Modules/Balance/Entity/GoldenTicketParamsEntity.swift
@@ -5,19 +5,21 @@
 //  Created by Fredy Leon on 18/4/26.
 //
 
+import Foundation
+
 public struct GoldenTicketParamsEntity: Encodable, Equatable, Sendable {
     public let recipientName: String
     public let taskDescription: String
     public let reward: String
     public let emailTo: String
-    public let memberId: String
+    public let memberId: UUID
     public let claimedValue: String
     
     public init(recipientName: String,
                 taskDescription: String,
                 reward: String,
                 emailTo: String,
-                memberId: String,
+                memberId: UUID,
                 claimedValue: String
     ) {
         self.recipientName = recipientName
