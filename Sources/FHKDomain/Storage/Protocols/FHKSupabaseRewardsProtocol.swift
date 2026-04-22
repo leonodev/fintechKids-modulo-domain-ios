683fa4544db3e1a1bacff9f7aaf625a0975d36f2
Fredy Leon <fredy.leon@ua.la.com>
2026-04-22 10:27:17 +0200

    remove parameter

--- Sources/FHKDomain/Storage/Protocols/FHKSupabaseRewardsProtocol.swift
+++ Sources/FHKDomain/Storage/Protocols/FHKSupabaseRewardsProtocol.swift
@@ -8,5 +8,5 @@
 public protocol FHKSupabaseRewardsProtocol: FHKInjectableProtocol {
     func createReward(reward: RewardEntity) async throws
     func fetchRewards(emailParent: String) async throws -> [RewardEntity]
-    func fetchRewardCollected(memberID: String, parentEmail: String) async throws -> [RewardCollectedEntity]
+    func fetchRewardCollected(parentEmail: String) async throws -> [RewardCollectedEntity]
 }
