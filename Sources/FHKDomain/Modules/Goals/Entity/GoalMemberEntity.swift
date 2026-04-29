2d7362de851859c01d12223d49c4cc0519ae4fd7
Fredy Leon <fredy.leon@ua.la.com>
2026-04-29 09:44:30 +0200

    add GoalMemberEntity

---
+++ Sources/FHKDomain/Modules/Goals/Entity/GoalMemberEntity.swift
@@ -0,0 +1,33 @@
+//
+//  GoalMemberEntity.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 29/4/26.
+//
+
+import Foundation
+
+public struct GoalMemberEntity: DomainModelProtocol {
+    public let id: UUID = UUID()
+    public let goalId: Int
+    public let memberId: UUID
+    public let rewardsSystemValue: Int
+    public let accumulatedValue: Int
+    public let taskWinnedValue: Int
+    public let rewardsSystemType: String
+    
+    public init(goalId: Int,
+                memberId: UUID,
+                rewardsSystemValue: Int,
+                accumulatedValue: Int,
+                taskWinnedValue: Int,
+                rewardsSystemType: String
+    ) {
+        self.goalId = goalId
+        self.memberId = memberId
+        self.rewardsSystemValue = rewardsSystemValue
+        self.accumulatedValue = accumulatedValue
+        self.taskWinnedValue = taskWinnedValue
+        self.rewardsSystemType = rewardsSystemType
+    }
+}
