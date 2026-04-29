4c1c102599eaebbf8a6b9217831bccb7db5170d2
Fredy Leon <fredy.leon@ua.la.com>
2026-04-29 11:21:37 +0200

    add new methods to protocols

--- Sources/FHKDomain/Modules/Goals/GoalRepositoryProtocol.swift
+++ Sources/FHKDomain/Modules/Goals/GoalRepositoryProtocol.swift
@@ -8,5 +8,7 @@
 public protocol FHKGoalRepositoryProtocol: FHKInjectableProtocol {
     func createGoal(goal: GoalEntity) async throws
     func getGoals(emailParent: String, forceRefresh: Bool) async throws -> [GoalEntity]
+    func createGoalMember(goal: GoalMemberEntity) async throws
+    func fetchGoalMember(member: MemberEntity, forceRefresh: Bool) async throws -> [GoalMemberEntity]
     func clearCache() async
 }
