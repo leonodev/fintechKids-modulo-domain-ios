e5a8e730160ebc8069d2d3e9f14bc9382d44c170
Fredy Leon <fredy.leon@ua.la.com>
2026-04-29 14:39:11 +0200

    re-order parameters

--- Sources/FHKDomain/Modules/Goals/Entity/GoalMemberEntity.swift
+++ Sources/FHKDomain/Modules/Goals/Entity/GoalMemberEntity.swift
@@ -11,23 +11,23 @@ public struct GoalMemberEntity: DomainModelProtocol {
     public let id: UUID = UUID()
     public let goalId: Int
     public let memberId: UUID
-    public let rewardsSystemValue: Int
     public let accumulatedValue: Int
     public let taskWinnedValue: Int
     public let rewardsSystemType: String
+    public let rewardsSystemValue: Int
     
     public init(goalId: Int,
                 memberId: UUID,
-                rewardsSystemValue: Int,
                 accumulatedValue: Int,
                 taskWinnedValue: Int = 0,
-                rewardsSystemType: String
+                rewardsSystemType: String,
+                rewardsSystemValue: Int,
     ) {
         self.goalId = goalId
         self.memberId = memberId
-        self.rewardsSystemValue = rewardsSystemValue
         self.accumulatedValue = accumulatedValue
         self.taskWinnedValue = taskWinnedValue
         self.rewardsSystemType = rewardsSystemType
+        self.rewardsSystemValue = rewardsSystemValue
     }
 }
