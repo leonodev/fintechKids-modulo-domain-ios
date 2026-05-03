2e41ae68ab4658cd415faaf1e913ea0b71faa4cc
Fredy Leon <fredy.leon@ua.la.com>
2026-05-03 08:10:25 +0200

    change parameters type

--- Sources/FHKDomain/Storage/Protocols/FHKSupabaseGoalProtocol.swift
+++ Sources/FHKDomain/Storage/Protocols/FHKSupabaseGoalProtocol.swift
@@ -5,9 +5,11 @@
 //  Created by Fredy Leon on 15/3/26.
 //
 
+import Foundation
+
 public protocol FHKSupabaseGoalProtocol: FHKInjectableProtocol {
     func createGoal(goal: GoalEntity) async throws
     func getGoals(emailParent: String) async throws -> [GoalEntity]
     func createGoalMember(goal: GoalMemberEntity) async throws
-    func fetchGoalMember(member: MemberEntity) async throws -> [GoalMemberEntity]
+    func fetchGoalMember(memberId: UUID) async throws -> [GoalMemberEntity]
 }
