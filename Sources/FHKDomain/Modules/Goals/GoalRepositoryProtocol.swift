cda24517dd245c3cc3e8220a001f2d725ae449be
Fredy Leon <fredy.leon@ua.la.com>
2026-05-03 08:19:24 +0200

    fix type parameters

--- Sources/FHKDomain/Modules/Goals/GoalRepositoryProtocol.swift
+++ Sources/FHKDomain/Modules/Goals/GoalRepositoryProtocol.swift
@@ -5,10 +5,12 @@
 //  Created by Fredy Leon on 15/3/26.
 //
 
+import Foundation
+
 public protocol FHKGoalRepositoryProtocol: FHKInjectableProtocol {
     func createGoal(goal: GoalEntity) async throws
     func getGoals(emailParent: String, forceRefresh: Bool) async throws -> [GoalEntity]
     func createGoalMember(goal: GoalMemberEntity) async throws
-    func fetchGoalMember(member: MemberEntity, forceRefresh: Bool) async throws -> [GoalMemberEntity]
+    func fetchGoalMember(memberId: UUID, forceRefresh: Bool) async throws -> [GoalMemberEntity]
     func clearCache() async
 }
