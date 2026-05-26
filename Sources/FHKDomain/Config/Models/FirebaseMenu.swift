89aea6faaa0f573a224792d84ee5d365a55b5428
Fredy Leon <feleon@knowmadmood.com>
2026-05-26 11:49:00 +0200

    add new model

---
+++ Sources/FHKDomain/Config/Models/FirebaseMenu.swift
@@ -0,0 +1,18 @@
+//
+//  FirebaseMenu.swift
+//  FHKDomain
+//
+//  Created by fleon  on 26/5/26.
+//
+
+public struct FirebaseMenuBottomHome {
+    public let bottom_bar_menus: [FirebaseMenuItem]
+}
+
+public struct FirebaseMenuItem {
+    public let id: Int
+    public let name: String
+    public let icon: String
+    public let label_localized_key: String
+    public let active: Bool
+}
