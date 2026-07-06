4cd6c57f266b911db74632df005c56e6bafb7ba3
Fredy Leon <feleon@knowmadmood.com>
2026-07-06 14:46:19 +0200

    change protocol by struct

--- Sources/FHKDomain/Modules/Splash/SplashRepositoryProtocol.swift
+++ Sources/FHKDomain/Modules/Splash/SplashRepositoryProtocol.swift
@@ -5,6 +5,28 @@
 //  Created by Fredy Leon on 1/3/26.
 //
 
-public protocol FHKSplashRepositoryProtocol: FHKInjectableProtocol {
-    func readLanguageCurrent() async throws -> String?
+//public protocol FHKSplashRepositoryProtocol: FHKInjectableProtocol {
+//    func readLanguageCurrent() async throws -> String?
+//}
+
+import Foundation
+
+public struct FHKSplashRepository: Sendable {
+    public var readLanguageCurrent: @Sendable () async throws -> String?
+    
+    public init(readLanguageCurrent: @escaping @Sendable () async throws -> String?) {
+        self.readLanguageCurrent = readLanguageCurrent
+    }
+}
+
+// Mocks por defecto: Como son estáticos y no dependen de bases de datos ni de la red,
+// pueden vivir aquí mismo en el dominio para que estén disponibles en todas partes.
+public extension FHKSplashRepository {
+    static var test: Self {
+        Self(readLanguageCurrent: { "es" })
+    }
+    
+    static var preview: Self {
+        Self(readLanguageCurrent: { "en" })
+    }
 }
