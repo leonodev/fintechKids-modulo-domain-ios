3a725913b8a40632241d8b44653d65ecba807cbd
Fredy Leon <feleon@knowmadmood.com>
2026-07-08 08:46:29 +0200

    restore loginRepository

---
+++ Sources/FHKDomain/Modules/Login/LoginRepository.swift
@@ -0,0 +1,28 @@
+//
+//  LoginRepositoryProtocol.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 1/3/26.
+//
+
+import Foundation
+
+public struct FHKLoginRepository: Sendable {
+    public var login: @Sendable (_ loginEntity: LoginEntity) async throws -> FHKUserSession? = { _ in nil }
+    public var loginWithBiometrics: @Sendable (_ prompt: String) async throws -> Void = { _ in }
+    public var saveAuthToken: @Sendable (_ token: String,
+                                         _ requiresBiometry: Bool) throws -> Void = { _, _ in }
+    public var saveUserIntoKeychain: @Sendable (_ email: String) async throws -> Void = { _ in }
+    public var savePinApproveTask: @Sendable (_ pin: String) async throws -> Void = { _ in }
+    public var refreshParentMail: @Sendable () -> Void = {}
+    public var hasSavedToken: @Sendable () -> Bool = { false }
+
+    public init() {}
+}
+
+
+public extension FHKLoginRepository {
+    static var test: Self {
+        Self()
+    }
+}
