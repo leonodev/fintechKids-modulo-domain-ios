5e027efdb82d0301dada79218c6b9177a82c5d90
Fredy Leon <feleon@knowmadmood.com>
2026-07-08 12:17:47 +0200

    add MainActor

--- Sources/FHKDomain/Commons/FHKToast.swift
+++ Sources/FHKDomain/Commons/FHKToast.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 public struct FHKToast: Sendable {
-    public var currentToast: @Sendable () -> FHKToastInfo? = { nil }
-    public var isVisible: @Sendable () -> Bool = { false }
+    public var currentToast: @MainActor @Sendable () -> FHKToastInfo? = { nil }
+    public var isVisible: @MainActor @Sendable () -> Bool = { false }
     public var show: @Sendable (FHKToastInfo, Double) -> Void = { _, _ in }
     public var dismiss: @Sendable () -> Void = { }
     
