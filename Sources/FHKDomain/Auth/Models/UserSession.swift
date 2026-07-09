b7e8fd2847859d68386a3f909c244febbac006f9
Fredy Leon <feleon@knowmadmood.com>
2026-07-09 10:37:46 +0200

    remove optionals

--- Sources/FHKDomain/Auth/Models/UserSession.swift
+++ Sources/FHKDomain/Auth/Models/UserSession.swift
@@ -4,20 +4,16 @@ import Foundation
 public struct FHKUserSession: Sendable {
     public let id: UUID
     public let email: String
-    public let accessToken: String?
-    public let refreshToken: String?
-    public let expiresAt: Date?
+    public let accessToken: String
+    public let refreshToken: String
+    public let expiresAt: Date
     public let infoAditional: InfoAditional?
-    
-    public var hasActiveSession: Bool {
-        return accessToken != nil
-    }
 
     public init(id: UUID,
                 email: String,
-                accessToken: String? = nil,
-                refreshToken: String? = nil,
-                expiresAt: Date? = nil,
+                accessToken: String,
+                refreshToken: String,
+                expiresAt: Date,
                 infoAditional: InfoAditional? = nil
     ) {
         self.id = id
