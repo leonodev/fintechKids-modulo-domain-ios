12f0460a2b072f5e0f4417bfd1d2730fde728eae
Fredy Leon <feleon@knowmadmood.com>
2026-07-20 09:59:57 +0200

    migrate protocol to Struct

--- Sources/FHKDomain/Core/Protocols/FHKServicesAPIProtocol.swift
+++ Sources/FHKDomain/Core/Protocols/FHKServicesAPIProtocol.swift
@@ -7,8 +7,23 @@
 
 import Foundation
 
-public protocol FHKServicesAPIProtocol: FHKInjectableProtocol {
-    func getURL(environment: EnvironmentType,
-                country: Countries,
-                serviceKey: ServiceType) throws -> String
+//public protocol FHKServicesAPIProtocol: FHKInjectableProtocol {
+//    func getURL(environment: EnvironmentType,
+//                country: Countries,
+//                serviceKey: ServiceType) throws -> String
+//}
+
+public struct FHKServices: Sendable {
+    public var getURL: @Sendable (EnvironmentType,
+                                  Countries,
+                                  ServiceType) throws -> String = { _,_,_ in "" }
+    
+    public init() {}
+}
+
+
+public extension FHKServices {
+    static var test: Self {
+        Self()
+    }
 }
