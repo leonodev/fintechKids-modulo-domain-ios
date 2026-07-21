ad9bbf83fc122eed9499690a71b1bc28cd01419b
Fredy Leon <feleon@knowmadmood.com>
2026-07-21 13:36:33 +0200

    refactor FHKSession

--- Sources/FHKDomain/Session/FHKSession+Test+Preview.swift
+++ Sources/FHKDomain/Session/FHKSession+Test+Preview.swift
@@ -18,11 +18,25 @@ import Foundation
 
 public struct FHKSession: Sendable {
     public var isAuthenticated: @Sendable () -> Bool = { false }
+    public var setIsAuthenticated: @Sendable (Bool) -> Void = { _ in }
+    
     public var initializeSession: @Sendable () async -> Void = { }
     public var login: @Sendable () async throws -> Void = { }
     public var logout: @Sendable () async throws -> Void = { }
     
-    public init() {}
+    public init(
+            isAuthenticated: @escaping @Sendable () -> Bool = { false },
+            setIsAuthenticated: @escaping @Sendable (Bool) -> Void = { _ in },
+            initializeSession: @escaping @Sendable () async -> Void = {},
+            login: @escaping @Sendable () async throws -> Void = {},
+            logout: @escaping @Sendable () async throws -> Void = {}
+    ) {
+        self.isAuthenticated = isAuthenticated
+        self.setIsAuthenticated = setIsAuthenticated
+        self.initializeSession = initializeSession
+        self.login = login
+        self.logout = logout
+    }
 }
 
 public extension FHKSession {
