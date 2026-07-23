1a32c4a463c8abcd0ab180670395ad75d5f0ea88
Fredy Leon <feleon@knowmadmood.com>
2026-07-23 11:54:38 +0200

    migrate FHKProfileRepository to struct

---
+++ Sources/FHKDomain/Modules/Profile/FHKProfileRepository+Test+Preview.swift
@@ -0,0 +1,43 @@
+//
+//  FHKProfileRepository+Test+Preview.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 8/3/26.
+//
+
+//public protocol FHKProfileRepositoryProtocol: FHKInjectableProtocol {
+//    func logout() async throws
+//    func deleteKeychain(key: String) throws
+//    func getEmailParent() throws -> String?
+//    func getLanguageCurrent() async -> String
+//    func setNewLanguage(lang: String)
+//    func getFamilyName() async -> String?
+//}
+
+public struct FHKProfileRepository: Sendable {
+    
+    public var logout: @Sendable() async throws -> Void = { }
+    public var deleteKeychain: @Sendable(String) throws -> Void = { _ in }
+    public var getEmailParent: @Sendable() throws -> String? = { nil }
+    public var getLanguageCurrent: @Sendable() async -> String = { "" }
+    public var setNewLanguage: @Sendable(String) -> Void = { _ in }
+    public var getFamilyName: @Sendable() async -> String? = { nil }
+    
+    public init() { }
+}
+
+public extension FHKProfileRepository {
+    
+    static var test: Self {
+        Self()
+    }
+    
+    static func preview(lng: LanguageType) -> Self {
+        var repository = Self()
+        repository.getEmailParent = { "user@domian.com" }
+        repository.getLanguageCurrent = { lng.code }
+        repository.getFamilyName = { "Family Dummy" }
+        
+        return repository
+    }
+}
