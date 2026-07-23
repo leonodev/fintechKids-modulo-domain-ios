5e7033740c1297fa06d839832ba5d634615c22a0
Fredy Leon <feleon@knowmadmood.com>
2026-07-23 08:58:58 +0200

    migrate to Struct instead Protocol

---
+++ Sources/FHKDomain/Modules/RegisterMember/FHKRegisterMembersRepository+Test+Preview.swift
@@ -0,0 +1,27 @@
+//
+//  FHKRegisterMembersRepository+Test+Preview.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 3/3/26.
+//
+
+//public protocol FHKRegisterMembersRepositoryProtocol: FHKInjectableProtocol {
+//    func registerMembers(members: [MemberEntity]) async throws
+//    func getParentMail() async -> String?
+//    func getFamilyName() async -> String?
+//}
+
+public struct FHKRegisterMembersRepository: Sendable {
+    public var registerMembers: @Sendable([MemberEntity]) async throws -> Void = { _ in }
+    public var getParentMail: @Sendable() async -> String? = { nil }
+    public var getFamilyName: @Sendable() async -> String? = { nil }
+    
+    public init() { }
+}
+
+public extension FHKRegisterMembersRepository {
+    
+    static var test: Self {
+        Self()
+    }
+}
