d145f16dfc8a7dd33d85b7d0694f4963787000b0
Fredy Leon <feleon@knowmadmood.com>
2026-07-24 12:20:10 +0200

    migrate FHKSupabaseGoal

---
+++ Sources/FHKDomain/Storage/Protocols/FHKSupabaseGoal+Test+Preview.swift
@@ -0,0 +1,60 @@
+//
+//  FHKSupabaseGoal+Test+Preview.swift
+//  FHKDomain
+//
+//  Created by Fredy Leon on 15/3/26.
+//
+
+import Foundation
+
+//public protocol FHKSupabaseGoalProtocol: FHKInjectableProtocol {
+//    func createGoal(goal: GoalEntity) async throws
+//    func getGoals(emailParent: String) async throws -> [GoalEntity]
+//    func createGoalMember(goal: GoalMemberEntity) async throws
+//    func fetchGoalMember(memberId: UUID) async throws -> [GoalMemberEntity]
+//    func fetchGoalMemberFamily(emailParent: String) async throws -> [GoalMemberEntity]
+//}
+
+public struct FHKSupabaseGoal: Sendable {
+    public var createGoal:
+    @Sendable(GoalEntity) async throws -> Void = { _ in }
+    
+    public var getGoals:
+    @Sendable(String) async throws -> [GoalEntity] = {_ in []}
+    
+    public var createGoalMember:
+    @Sendable(GoalMemberEntity) async throws -> Void = {_ in }
+    
+    public var fetchGoalMember:
+    @Sendable(UUID) async throws -> [GoalMemberEntity] = {_ in []}
+    
+    public var fetchGoalMemberFamily:
+    @Sendable(String) async throws -> [GoalMemberEntity] = {_ in []}
+    
+    public init() {}
+}
+
+public extension FHKSupabaseGoal {
+    
+    static var test: Self {
+        Self()
+    }
+    
+    static var preview: Self {
+        var goals = Self()
+        
+        goals.createGoal = { _ in }
+        goals.getGoals = { emailParent in
+            [
+                GoalEntity(expirationDate: Date().toUTC,
+                           name: "PSP5",
+                           emailParent: "parent@domain.com",
+                           value: 900,
+                           measureType: "coins",
+                           status: .inCurse)
+            ]
+        }
+        
+        return goals
+    }
+}
